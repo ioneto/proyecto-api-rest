b75a18c0ec7e512c6a94680bdb4392184f61ff59
Felipe Mancilla <felipe.mancilla@alumnos.usm.cl>
2016-10-23 01:14:59 -0300

    Correción API REST

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
-  protect_from_forgery with: :null_session
+  #protect_from_forgery with: :null_session
 
   before_filter :add_allow_credentials_headers
 
